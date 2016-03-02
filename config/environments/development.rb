920caf4bf0d9291db97267e6070449db8a199820
csdevs1 <csdevs1@gmail.com>
2016-03-01 20:07:54 -0430

    configured ENV Variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,20 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+    
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default :charset => "utf-8"  
+    # SMTP settings for gmail
+    config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :user_name            => ENV['EMAIL_ACCOUNT'],
+        :password             => ENV['MAIL_PSWD'],
+        :authentication       => "plain",
+        :enable_starttls_auto => true
+        }
+
+    
   config.cache_classes = false
 
   # Do not eager load code on boot.
